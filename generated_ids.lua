1a74d0da0c0cf342c2cdd7739add69a619b254c6
Minotorious <minotorious@gmail.com>
2021-02-01 20:49:03 +0100

    assemblage mode

--- generated_ids.lua
+++ generated_ids.lua
@@ -5,6 +5,8 @@ ModId = "4fcea8ec-a6e7-41cd-8075-ae4bf4c00ecd"
 
 IdMap = {
 	{ "WATER_CRANE", "BUILDING", "e0450ad4-1a7a-43bb-9cd0-c0fa852e77aa" },
+	{ "GEAR_MIDDLE_BACK_PART", "BUILDING_PART", "2a81cc47-5089-4a96-8296-2e186354a9b6" },
+	{ "GEAR_TOP_BACK_PART", "BUILDING_PART", "b488b22d-f471-4551-bda2-99b8cb029b9e" },
 	{ "WATER_CRANE_PART", "BUILDING_PART", "a9ee9ab3-428e-474f-9ed4-494e39aa7eb2" },
 	{ "models/metalSilver.png", "", "3939b35a-e6bb-4882-86ef-81a02048085a" },
 	{ "models/ropeBrown.jpg", "", "4062016f-5c07-4e46-8905-48b43fce4112" },
@@ -13,8 +15,17 @@ IdMap = {
 	{ "models/waterCrane.fbx", "", "8a7a860e-6748-4bdc-b47c-4bb860b608d3" },
 	{ "models/metalRusty.png", "", "b53efb9f-4c89-40b4-8e28-98c6b40e718e" },
 	{ "models/woodBrown.png", "", "9a9ca03c-8ad5-4c29-97c3-dc3f53d5fe53" },
+	{ "GEAR_MAIN_BACK_PART", "BUILDING_PART", "b5a27a09-8a4d-49ba-a670-e87185150b9d" },
 	{ "models/woodBrownDark.png", "", "b5467e49-f79b-4b4c-9d12-f52e1e3432a5" },
 	{ "models/woodBrownWorn.png", "", "94588350-011e-4c1c-be52-564b61c35787" },
 	{ "models/woodplanksBrown.png", "", "92e57491-6092-42c8-a50b-d108f1441e37" },
 	{ "models/craneRender.png", "", "97fb342d-09cf-4ed8-8016-34740b787871" },
+	{ "models/craneRenderSquare.png", "", "05ebaf0e-b7a4-48c8-b964-56d0a65abfd3" },
+	{ "GEAR_MAIN_PART", "BUILDING_PART", "296d12cd-afb9-457b-9857-c90de5f78ddb" },
+	{ "GEAR_MAIN_FRONT_PART", "BUILDING_PART", "fc65ccb4-37c9-4196-80da-dcb58278becb" },
+	{ "GEAR_MIDDLE_PART", "BUILDING_PART", "11ed1bc3-f2a7-4b90-b169-857b0f7ef3e6" },
+	{ "GEAR_TOP_PART", "BUILDING_PART", "e1ad350d-91bf-4204-a94d-5fcd26ffa0c7" },
+	{ "GEAR_MIDDLE_FRONT_PART", "BUILDING_PART", "e63c6970-be5e-4ee8-984d-02d7f4a5f160" },
+	{ "GEAR_TOP_FRONT_PART", "BUILDING_PART", "dc6cac97-fb0b-4322-8bf9-6a5bc4fdd091" },
+	{ "TREADMILL_PART", "BUILDING_PART", "a89a8ed3-d11d-4014-a6ef-5e6fc6f99065" },
 }
--- mod.lua
+++ mod.lua
@@ -8,7 +8,61 @@
 
 local waterCrane = foundation.createMod();
 
-waterCrane:registerAssetId("models/waterCrane.fbx/Prefab/waterCranePart", "PREFAB_WATER_CRANE_PART")
+waterCrane:registerAssetId("models/waterCrane.fbx/Prefab/GearMainPart", "PREFAB_GEAR_MAIN_PART")
+waterCrane:registerAssetId("models/waterCrane.fbx/Prefab/GearMainFrontPart", "PREFAB_GEAR_MAIN_FRONT_PART")
+waterCrane:registerAssetId("models/waterCrane.fbx/Prefab/GearMainBackPart", "PREFAB_GEAR_MAIN_BACK_PART")
+waterCrane:registerAssetId("models/waterCrane.fbx/Prefab/GearTopPart", "PREFAB_GEAR_TOP_PART")
+waterCrane:registerAssetId("models/waterCrane.fbx/Prefab/GearTopFrontPart", "PREFAB_GEAR_TOP_FRONT_PART")
+waterCrane:registerAssetId("models/waterCrane.fbx/Prefab/GearTopBackPart", "PREFAB_GEAR_TOP_BACK_PART")
+waterCrane:registerAssetId("models/waterCrane.fbx/Prefab/GearMiddlePart", "PREFAB_GEAR_MIDDLE_PART")
+waterCrane:registerAssetId("models/waterCrane.fbx/Prefab/GearMiddleFrontPart", "PREFAB_GEAR_MIDDLE_FRONT_PART")
+waterCrane:registerAssetId("models/waterCrane.fbx/Prefab/GearMiddleBackPart", "PREFAB_GEAR_MIDDLE_BACK_PART")
+waterCrane:registerAssetId("models/waterCrane.fbx/Prefab/TreadmillPart", "PREFAB_TREADMILL_PART")
+waterCrane:registerAssetId("models/waterCrane.fbx/Prefab/WaterCraneCorePart", "PREFAB_WATER_CRANE_CORE_PART")
+
+waterCrane:registerAssetProcessor("models/waterCrane.fbx", { DataType = "BUILDING_ASSET_PROCESSOR" })
+
+
+
+function registerDefaultBuildingPart(_nodePrefix)
+	waterCrane:register({
+		DataType = "BUILDING_PART",
+		Id = _nodePrefix .. "_PART",
+		ConstructorData = { DataType = "BUILDING_CONSTRUCTOR_DEFAULT", CoreObjectPrefab = "PREFAB_" .. _nodePrefix .. "_PART" }
+	})
+end
+
+local defaultNodePrefixList = {
+    "GEAR_MAIN",
+    "GEAR_MAIN_FRONT",
+    "GEAR_MAIN_BACK",
+    "GEAR_TOP",
+    "GEAR_TOP_FRONT",
+    "GEAR_TOP_BACK",
+    "GEAR_MIDDLE",
+    "GEAR_MIDDLE_FRONT",
+    "GEAR_MIDDLE_BACK",
+    "TREADMILL"
+}
+
+for i, nodePrefix in ipairs(defaultNodePrefixList) do
+	registerDefaultBuildingPart(nodePrefix)
+end
+
+waterCrane:registerPrefabComponent("models/waterCrane.fbx/Prefab/GearMainPart", { DataType = "COMP_BUILDING_PART", BuildingPartType = "BUILD_ORDER_DEPENDENCY" })
+waterCrane:registerPrefabComponent("models/waterCrane.fbx/Prefab/GearMainFrontPart", { DataType = "COMP_BUILDING_PART", BuildingPartType = "LEFT" })
+waterCrane:registerPrefabComponent("models/waterCrane.fbx/Prefab/GearMainBackPart", { DataType = "COMP_BUILDING_PART", BuildingPartType = "RIGHT" })
+waterCrane:registerPrefabComponent("models/waterCrane.fbx/Prefab/GearTopPart", { DataType = "COMP_BUILDING_PART", BuildingPartType = "INNER_BOTTOM" })
+waterCrane:registerPrefabComponent("models/waterCrane.fbx/Prefab/GearTopFrontPart", { DataType = "COMP_BUILDING_PART", BuildingPartType = "FRONT" })
+waterCrane:registerPrefabComponent("models/waterCrane.fbx/Prefab/GearTopBackPart", { DataType = "COMP_BUILDING_PART", BuildingPartType = "SIDE" })
+waterCrane:registerPrefabComponent("models/waterCrane.fbx/Prefab/GearMiddlePart", { DataType = "COMP_BUILDING_PART", BuildingPartType = "MAJOR" })
+waterCrane:registerPrefabComponent("models/waterCrane.fbx/Prefab/GearMiddleFrontPart", { DataType = "COMP_BUILDING_PART", BuildingPartType = "MINOR" })
+waterCrane:registerPrefabComponent("models/waterCrane.fbx/Prefab/GearMiddleBackPart", { DataType = "COMP_BUILDING_PART", BuildingPartType = "INNER_TOP" })
+waterCrane:registerPrefabComponent("models/waterCrane.fbx/Prefab/TreadmillPart", { DataType = "COMP_BUILDING_PART", BuildingPartType = "DOOR" })
+
+waterCrane:registerPrefabComponent("models/waterCrane.fbx/Prefab/WaterCraneCorePart/AttachBuildOrderDependency", { DataType = "COMP_BUILDING_ATTACH_NODE", AttachNodeTypeList = { "BUILD_ORDER_DEPENDENCY" } })
+waterCrane:registerPrefabComponent("models/waterCrane.fbx/Prefab/WaterCraneCorePart/AttachFront", { DataType = "COMP_BUILDING_ATTACH_NODE", AttachNodeTypeList = { "FRONT" } })
+waterCrane:registerPrefabComponent("models/waterCrane.fbx/Prefab/WaterCraneCorePart/AttachSide", { DataType = "COMP_BUILDING_ATTACH_NODE", AttachNodeTypeList = { "SIDE" } })
 
 waterCrane:register({
 	DataType = "BUILDING",
@@ -25,14 +79,34 @@ waterCrane:register({
     Name = "WATER_CRANE_PART_NAME",
 	Description = "WATER_CRANE_PART_DESC",
 	ConstructorData = {
-		DataType = "BUILDING_CONSTRUCTOR_DEFAULT",
-		CoreObjectPrefab = "PREFAB_WATER_CRANE_PART"
+        DataType = "BUILDING_CONSTRUCTOR_ASSEMBLAGE",
+        CoreRandomBuildingPartList = { "PREFAB_WATER_CRANE_CORE_PART" },
+        MandatoryBuildingPartList = {
+            { BuildingPart = "GEAR_MAIN_PART" },
+            { BuildingPart = "GEAR_MAIN_FRONT_PART" },
+            { BuildingPart = "GEAR_MAIN_BACK_PART" },
+            { BuildingPart = "GEAR_TOP_PART" },
+            { BuildingPart = "GEAR_TOP_FRONT_PART" },
+            { BuildingPart = "GEAR_TOP_BACK_PART" },
+            { BuildingPart = "GEAR_MIDDLE_PART" },
+            { BuildingPart = "GEAR_MIDDLE_FRONT_PART" },
+            { BuildingPart = "GEAR_MIDDLE_BACK_PART" },
+            { BuildingPart = "TREADMILL_PART" }
+        }
 	},
 	BuildingZone = {
 		ZoneEntryList = {
             {
             Polygon = polygon.createRectangle( { 6, 10 }, { 0, 0 } ),
             Type = { DEFAULT = true, NAVIGABLE = false, GRASS_CLEAR = true }
+            },
+            {
+            Polygon = polygon.createRectangle( { .5, .5 }, { 4, 2 } ),
+            Type = { WATER_CONSTRAINT = true }
+            },
+            {
+            Polygon = polygon.createRectangle( { 1, 1 }, { 7, 2 } ),
+            Type = { DEFAULT = true, NAVIGABLE = true }
             }
         }
 	},
@@ -46,6 +120,23 @@ waterCrane:register({
 --[[---------------------------- GENERAL MATERIALS ----------------------------]]--
 
 --[[---------------------------- CUSTOM COMPONENTS ----------------------------]]--
+
+local COMP_WATER_CRANE = {
+	TypeName = "COMP_WATER_CRANE",
+	ParentType = "COMPONENT",
+	Properties = {
+		{ Name = "GearVelocity", Type = "float", Default = 0.3 }
+	}
+}
+
+function COMP_WATER_CRANE:create()
+	self.sequence = 0
+end
+
+function COMP_WATER_CRANE:generalSequence()
+    local dt = self:getLevel():getDeltaTime()
+    
+end
 
 --[[------------------------------ MAIN MONUMENT ------------------------------]]--
 

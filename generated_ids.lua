7f239d69f7ebcfc1466ac5354d4c3ed86260290c
Minotorious <minotorious@gmail.com>
2021-02-01 02:17:18 +0100

    crane model preliminary finished

---
+++ generated_ids.lua
@@ -0,0 +1,19 @@
+-- This is a generated file
+-- Do not modify it, unless you know exactly what you are doing
+
+ModId = "4fcea8ec-a6e7-41cd-8075-ae4bf4c00ecd"
+
+IdMap = {
+	{ "WATER_CRANE", "BUILDING", "e0450ad4-1a7a-43bb-9cd0-c0fa852e77aa" },
+	{ "WATER_CRANE_PART", "BUILDING_PART", "a9ee9ab3-428e-474f-9ed4-494e39aa7eb2" },
+	{ "models/metalSilver.png", "", "3939b35a-e6bb-4882-86ef-81a02048085a" },
+	{ "models/ropeBrown.jpg", "", "4062016f-5c07-4e46-8905-48b43fce4112" },
+	{ "models/stoneGrey.png", "", "d3f409b3-54fc-4dcd-bfe3-6c6de6abdc76" },
+	{ "models/untitled.png", "", "e473a964-b42a-4044-aaa8-c7734928b2cd" },
+	{ "models/waterCrane.fbx", "", "8a7a860e-6748-4bdc-b47c-4bb860b608d3" },
+	{ "models/metalRusty.png", "", "b53efb9f-4c89-40b4-8e28-98c6b40e718e" },
+	{ "models/woodBrown.png", "", "9a9ca03c-8ad5-4c29-97c3-dc3f53d5fe53" },
+	{ "models/woodBrownDark.png", "", "b5467e49-f79b-4b4c-9d12-f52e1e3432a5" },
+	{ "models/woodBrownWorn.png", "", "94588350-011e-4c1c-be52-564b61c35787" },
+	{ "models/woodplanksBrown.png", "", "92e57491-6092-42c8-a50b-d108f1441e37" },
+}
--- mod.lua
+++ mod.lua
@@ -8,6 +8,41 @@
 
 local waterCrane = foundation.createMod();
 
+waterCrane:registerAssetId("models/waterCrane.fbx/Prefab/waterCranePart", "PREFAB_WATER_CRANE_PART")
+
+waterCrane:register({
+	DataType = "BUILDING",
+	Id = "WATER_CRANE",
+	Name = "WATER_CRANE_NAME",
+	Description = "WATER_CRANE_DESC",
+	BuildingType = "DECORATION",
+	AssetCoreBuildingPart = "WATER_CRANE_PART"
+})
+
+waterCrane:register({
+	DataType = "BUILDING_PART",
+	Id = "WATER_CRANE_PART",
+    Name = "WATER_CRANE_PART_NAME",
+	Description = "WATER_CRANE_PART_DESC",
+	ConstructorData = {
+		DataType = "BUILDING_CONSTRUCTOR_DEFAULT",
+		CoreObjectPrefab = "PREFAB_WATER_CRANE_PART"
+	},
+	BuildingZone = {
+		ZoneEntryList = {
+            {
+            Polygon = polygon.createRectangle( { 6, 10 }, { 0, 0 } ),
+            Type = { DEFAULT = true, NAVIGABLE = false, GRASS_CLEAR = true }
+            }
+        }
+	},
+	ConstructionVisual = nil,
+	Cost = {
+		RessourcesNeeded = {}
+	},
+    IsVisibleWhenBuilt = true
+})
+
 --[[---------------------------- GENERAL MATERIALS ----------------------------]]--
 
 --[[---------------------------- CUSTOM COMPONENTS ----------------------------]]--

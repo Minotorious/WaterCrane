95decfc9eca21e47c3b3c545026df804485cb0e3
Minotorious <minotorious@gmail.com>
2021-02-06 23:17:26 +0100

    Update waterCrane.lua

--- scripts/waterCrane.lua
+++ scripts/waterCrane.lua
@@ -648,7 +648,7 @@ waterCrane:register({
     WorkerCapacity = 1,
     RelatedJob = { Job = "TREADMILL_WORKER", Behavior = "TREADMILL_WORKER_BEHAVIOR_TREE" },
     ResourceProduced = {
-        { Resource = "FISH", Quantity = 5 }
+        { Resource = "FISH", Quantity = 10 }
     },
     RandomResourceToSpawn = "GEMS",
     SpawnPercentage = 0.33
